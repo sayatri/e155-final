ecb935d4420bc53e5df4fecc10305b72bd544386
sayatri <sarahtrisorus@gmail.com>
2014-12-06 01:20:00 -0800

    Updated speechrec module for FPGA (works in ModelSim)

--- speechrec.sv
+++ speechrec.sv
@@ -10,7 +10,7 @@ module speechrec(input  logic       clk, sck, sdi, ss, reset,
 					  output logic [3:0] result); //TODO
 
     // Extra signals
-	 logic data_ready;      // Keep track of whether or not 32-bit data is ready to be stored
+	 logic data_exchanged;  // Keep track of whether or not all data has been exchanged via SPI
     logic input_ready;     // Keep track of whether or not input audio has been received and
                            //   processing is ready to begin
     logic transmit_ready;  // Keep track of whether or not processing is complete and data
@@ -36,22 +36,26 @@ module speechrec(input  logic       clk, sck, sdi, ss, reset,
     always_comb begin
         case(state)
             S0: nextstate =             ss ? S1 : S0; // S0: Wait for master to send input audio
-            S1: nextstate =    input_ready ? S0 : S1; // S1: Receive the audio
+            S1: nextstate =    input_ready ? S3 : S1; // S1: Receive the audio
             S2: nextstate = transmit_ready ? S3 : S2; // S2: Process the audio
             S3: nextstate =             ss ? S4 : S3; // S3: Wait for master to be ready
-            S4: nextstate =  transmit_done ? S0 : S4; // S4: Transmit the audio
+            S4: nextstate = data_exchanged ? S0 : S4; // S4: Transmit the audio
             default: nextstate = S0;
        endcase
     end
     
-	 assign led[2:0] = S0;
-	 assign led[5:3] = nextstate;
+	 assign led = state;
 	 
    // spi_send_receive receiver(state, sck, sdo, sdi, reset, junk_send, data_receive, input_ready);
 	// spi_send_receive receiver(state, sck, sdi, sdo, reset, data_send, data_receive);
-	spi_send_receive receiver(state, sck, sdi, sdo, data_send, data_receive, data_ready);
-	assign data_send = data_receive;
-	compile_audio compiler(clk, state, data_receive, data_ready, audio, input_ready);
+	spi_send_receive comm(state, sck, sdi, sdo, data_send, data_receive, data_exchanged);
+	compile_audio compiler(clk, state, data_receive, data_exchanged, audio, input_ready);
+	
+	// TODO: Temporarily transmit a 10-bit logic
+	logic [9:0] res;
+	assign res = 10'b1010101010;
+	assign data_send = {22'b0, res};
+	
     //assign led = data_receive;
      // FIXME vvv
      //store store_int(state, sck, data_receive, input_ready, audio);
@@ -65,7 +69,7 @@ endmodule
 module compile_audio(input  logic        clk,
                      input  logic [2:0]  state,
                      input  logic [31:0] data,
-                     input  logic        data_ready,
+                     input  logic        data_exchanged,
                      output logic [9:0]  audio[1999:0],
 						   output logic        input_ready);
 
@@ -73,17 +77,14 @@ module compile_audio(input  logic        clk,
 							
   always_ff @(posedge clk) begin
     // Reset the counter if new data is going to be received
-    if (state == 3'b000) begin
+    if (state == 3'b111) begin
 	   cnt <= 0;
 	 end
 
 	 // Add to the audio array until all data has been added
-    else if (data_ready && cnt < 11'd2000) begin
+    else if (data_exchanged && cnt < 11'd2000) begin
 	   audio[cnt] <= data[9:0];
 		cnt <= cnt + 11'b1;
-	 end
-	 else begin
-	   cnt <= 11'd2001;   // Make sure the counter doesn't overflow
 	 end
 	 
 	 // Set the flag when all the data has been added
@@ -105,14 +106,18 @@ module spi_send_receive(input  logic [2:0]  state,
   
   always_ff @(negedge sck) begin	 
     // set flag and store data after data fully exchanged
-	 if (cnt >= 5'd31) begin
+	 if (state == 3'b001 && cnt >= 5'd31) begin
 	   q   <= buffer;
 	 end
 	 
 	 // increase count if in a state where the module is active
+	 // reset count if in a waiting state
 	 if (state == 3'b001 || state == 3'b100) begin
 	   cnt <= cnt + 5'b1;
 	 end
+	 else if (state == 3'b111 || state == 3'b011) begin
+	   cnt <= 0;
+    end
   end
   
   always_comb begin
