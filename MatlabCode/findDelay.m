a13de804f34bab32afefe82a6eba48773ed8ec50
mngai <mngai@hmc.edu>
2014-12-06 01:00:55 -0800

    added finding delay for correlation

---
+++ MatlabCode/findDelay.m
@@ -0,0 +1,51 @@
+close all
+% Control to make sure correlation is correct
+a = [0 0 0 0 0 2 3 0];
+b = [0 0 0 2 3 0 0 0];
+
+figure
+subplot(2,1,1)
+plot(a)
+title('a');
+subplot(2,1,2)
+plot(b)
+title('b');
+% 
+% matlabXcorr = xcorr(a, b)
+% figure
+% plot(matlabXcorr)
+% title('Result of matlab xcorr');
+
+%
+%
+% figure
+% plot(conv(a,fliplr(b)))
+% title('Result of xcorr with conv');
+
+% DO NOT CHANGE THIS IS PERFECT %%
+result = zeros(2*length(a) - 1,1);
+for n=1:(2*length(a) - 1)
+    for k = 0:n-1
+        if n-k <= length(a)
+            ca = a(n-k);
+        else
+            ca = 0;
+        end
+        
+        if ((length(b)-k) > 0)
+            cb = b(end-k);
+        else
+            cb = 0;
+        end
+        result(n)= result(n) + ca*cb;
+     
+    end
+end
+
+figure
+plot(result)
+title('Result of xcorr with for loop');
+
+[~,indexMax] = max(abs(result));
+
+delay = ceil(length(result)/2) - indexMax
--- work/compare@from@bank/_primary.vhd
+++ work/compare@from@bank/_primary.vhd
@@ -4,8 +4,10 @@ entity compareFromBank is
     port(
         state           : in     vl_logic_vector(2 downto 0);
         clk             : in     vl_logic;
-        resultCompare   : out    vl_logic;
-        finalIndex      : out    vl_logic;
+        resultOne       : out    vl_logic_vector(14 downto 0);
+        resultTwo       : out    vl_logic_vector(14 downto 0);
+        maxIndexOne     : out    vl_logic_vector(11 downto 0);
+        maxIndexTwo     : out    vl_logic_vector(11 downto 0);
         transmit_ready  : out    vl_logic
     );
 end compareFromBank;
